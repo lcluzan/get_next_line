cc1533b513579c64f3e658f0d2c81cd93c1f8003
lcluzan <lcluzan@student.42.fr>
2024-07-16 14:58:30 +0200

    modif header

--- get_next_line.h
+++ get_next_line.h
@@ -6,26 +6,33 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 15:10:26 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/07/08 11:45:13 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/07/16 14:15:14 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE n
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 20
+# endif
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
 
+/*utils functions*/
+char	*ft_strdup(const char *s1);
 size_t	ft_strlen(const char *str);
 char	*ft_substr(char const *s, size_t start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strdup(const char *s1);
 char	*ft_strchr(const char *s, int c);
+
+/*GNL functions*/
+char	*stash_filling(int fd, char *stash, char *buffer);
+char	*extract_new_stash(char	*stash);
+char	*extract_line(char *stash, char *line);
+char	*get_next_line(int fd);
 
 #endif
